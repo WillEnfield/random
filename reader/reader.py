e86f74f4011f1cb9448a4414d3187b19ebece82e
Will Enfield <williamenfield@outlook.com>
2025-08-22 17:45:00 -0700

    added file selector

--- reader/reader.py
+++ reader/reader.py
@@ -1,4 +1,24 @@
-path = input("What so you want to read?\n")
+import subprocess
+
+path = None
+while(True):
+    if path == None:
+        path = input("What file do you want to select?\n")
+        try:
+            with open(path, 'r') as file:
+                pass
+        except FileNotFoundError:
+            print("Thats not a valid path.")
+            path = None
+        else:
+            if path[-4:] != ".txt":
+                print("Thats not a txt file. Please select a txt file.")
+                path = None
+    else:
+        pass
+        #once file is selected we go here
+
+
 with open(path, 'r') as file:
     content = file.read()
     print(content)
