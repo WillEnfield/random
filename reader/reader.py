6c6d2f06d1bc3ba274f51f8fe8109f766fe1566f
Will Enfield <williamenfield@outlook.com>
2025-08-23 17:34:03 -0700

    added better cd and basic commands

--- reader/reader.py
+++ reader/reader.py
@@ -1,24 +1,26 @@
-import subprocess
+import os
 
-path = None
+path = "C:\\" 
 while(True):
-    if path == None:
-        path = input("What file do you want to select?\n")
-        try:
-            with open(path, 'r') as file:
-                pass
-        except FileNotFoundError:
-            print("Thats not a valid path.")
-            path = None
+        command = input(path + "> ")
+        if command == "help":
+            print("Commands:\nhelp - show this message\nopen - open the file in the default editor\nend - end the program\nselect (path) - select a new file")
+        elif command == "end":
+            break
+        elif command == "open":
+            os.startfile(path)
+        elif command.startswith("cd "):
+            if path == "..":
+                newpath = os.path.dirname(path)
+            elif os.path.isabs(command[3:].strip()):
+                new_path = command[3:].strip()
+            else:
+                new_path = os.path.join(os.path.dirname(path), command[3:].strip())
+            if os.path.exists(new_path):
+                path = os.path.abspath(new_path)
+            else:
+                print("That's not a valid path.")
+        elif command == "":
+            continue
         else:
-            if path[-4:] != ".txt":
-                print("Thats not a txt file. Please select a txt file.")
-                path = None
-    else:
-        pass
-        #once file is selected we go here
-
-
-with open(path, 'r') as file:
-    content = file.read()
-    print(content)
+            print("Unknown command. Type 'help' for a list of commands.")
