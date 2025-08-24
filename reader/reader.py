68a326729a8cc51abbc44c876bd444cbe71bd7de
Will Enfield <williamenfield@outlook.com>
2025-08-23 18:08:32 -0700

    made cd not select files, open broken.

--- reader/reader.py
+++ reader/reader.py
@@ -4,20 +4,23 @@ path = "C:\\"
 while(True):
         command = input(path + "> ")
         if command == "help":
-            print("Commands:\nhelp - show this message\nopen - open the file in the default editor\nend - end the program\nselect (path) - select a new file")
+            print("Commands:\nhelp - show this message\nopen - open the file in the default editor\nend - end the program\ncd (path) - select a new path")
         elif command == "end":
             break
         elif command == "open":
             os.startfile(path)
         elif command.startswith("cd "):
-            if path == "..":
-                newpath = os.path.dirname(path)
+            if command[3:].strip() == "..":
+                new_path = os.path.dirname(path)
             elif os.path.isabs(command[3:].strip()):
                 new_path = command[3:].strip()
             else:
-                new_path = os.path.join(os.path.dirname(path), command[3:].strip())
+                new_path = os.path.join(path, command[3:].strip())
             if os.path.exists(new_path):
-                path = os.path.abspath(new_path)
+                if not os.path.isfile(new_path):
+                    path = os.path.abspath(new_path)
+                else:
+                    print("Cannot select a file, please select a directory.")
             else:
                 print("That's not a valid path.")
         elif command == "":
