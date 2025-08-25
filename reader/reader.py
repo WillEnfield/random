64017110f87e306012d1510fd9e051b34ada51a9
Will Enfield <williamenfield@outlook.com>
2025-08-24 20:12:40 -0700

    made easier to read and restored functionality to open

--- reader/reader.py
+++ reader/reader.py
@@ -1,29 +1,39 @@
 import os
 
-path = "C:\\" 
+workingdir = "C:\\" 
 while(True):
-        command = input(path + "> ")
-        if command == "help":
-            print("Commands:\nhelp - show this message\nopen - open the file in the default editor\nend - end the program\ncd (path) - select a new path")
-        elif command == "end":
-            break
-        elif command == "open":
-            os.startfile(path)
-        elif command.startswith("cd "):
-            if command[3:].strip() == "..":
-                new_path = os.path.dirname(path)
-            elif os.path.isabs(command[3:].strip()):
-                new_path = command[3:].strip()
-            else:
-                new_path = os.path.join(path, command[3:].strip())
-            if os.path.exists(new_path):
-                if not os.path.isfile(new_path):
-                    path = os.path.abspath(new_path)
-                else:
-                    print("Cannot select a file, please select a directory.")
-            else:
-                print("That's not a valid path.")
-        elif command == "":
-            continue
+    command = input(workingdir + "> ")
+    if command == "help":
+        print("Commands:\nhelp - show this message\nopen (path)- open the file in the default program\nend - end the program\ncd (path) - select a new path\ndir (path)* - List the content of the directory\n *optional")
+    elif command == "end":
+         break
+    elif command.startswith("open "):
+        if os.path.isabs(command[5:].strip()):
+            file = command[5:].strip()
         else:
-            print("Unknown command. Type 'help' for a list of commands.")
+            file = os.path.join(workingdir, command[5:].strip())
+        if os.path.exists(file):
+            if os.path.isfile(file):
+                os.startfile(file)
+            else:
+                print("The selected path is a directory, not a file.")
+        else:
+            print("That's not a valid path.")
+    elif command.startswith("cd "):
+        if command[3:].strip() == "..":
+            new_workingdir = os.path.dirname(workingdir)
+        elif os.path.isabs(command[3:].strip()):
+            new_workingdir = command[3:].strip()
+        else:
+            new_workingdir = os.path.join(workingdir, command[3:].strip())
+        if os.path.exists(new_workingdir):
+            if not os.path.isfile(new_workingdir):
+                workingdir = os.path.abspath(new_workingdir)
+            else:
+                print("Cannot select a file, please select a directory.")
+        else:
+            print("That's not a valid path.")
+    elif command == "":
+        continue
+    else:
+        print("Unknown command. Type 'help' for a list of commands.")
